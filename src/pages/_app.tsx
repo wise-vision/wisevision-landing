89dc8adc8a49175c20e57da1d927bd7a4ee9ea89
Rafał <nedvedek111@gmail.com>
2021-06-20 14:26:52 +0200

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,47 @@
+import { Global } from '@emotion/core';
+import { AppProps } from 'next/app';
+import React from 'react';
+import theme from 'theme';
+import { ThemeProvider } from 'theme-ui';
+import Head from 'next/head';
+
+const globalStyles = `
+  html,
+  body,
+  body > div:first-of-type,
+  div#__next {
+    height: 100%;
+    width: 100%;
+  }
+
+  html {
+    overflow-x: hidden;
+  }
+
+  div#__next {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
+  }
+
+  body.ReactModal__Body--open {
+    overflow: hidden;
+  }
+`;
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider {...{ theme }}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
