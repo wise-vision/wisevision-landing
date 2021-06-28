3d5f59343e4931762e3824498a2ef2249cd9c118
pacxiu <nedvedek111@gmail.com>
2021-06-28 17:59:54 +0200

    Add additional attributes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,6 +37,8 @@ function HeroSection() {
         autoPlay
         muted
         loop
+        playsinline
+        disablePictureInPicture
         id="myVideo"
         poster="static/home/hero.jpg"
         sx={{
