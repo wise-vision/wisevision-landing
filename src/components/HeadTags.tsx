967b1404a4bd6e4c378a5d2f431ea6741bf5a9c5
pacxiu <nedvedek111@gmail.com>
2021-08-20 11:45:10 +0200

    Add google tag manager

--- src/components/HeadTags.tsx
+++ src/components/HeadTags.tsx
@@ -1,21 +1,23 @@
 /* eslint-disable react/no-danger */
 import Head from 'next/head';
 
-function GoogleAnalytics() {
+function GoogleTagManager() {
   return (
     <>
       <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <script
         defer
         dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_CODE}');`,
+        }}
+      />
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_CODE}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         }}
       />
     </>
@@ -49,7 +51,7 @@ export function BaseHeadTags() {
         href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;600;700&display=swap"
         rel="stylesheet"
       />
-      <GoogleAnalytics />
+      <GoogleTagManager />
       <LiveChat />
     </>
   );
